ed67f5129e94b89a08e8a912415e286e36990632
Aleksandr Esin <aleksandr.esin@glowbyteconsulting.com>
2022-11-03 13:40:10 +0300

    Les 3

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,13 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if (typeof obj === 'object'){
+        const myObj = {};
+        for (const [key, value] of Object.entries(obj)){
+          myObj[value] = key;
+        }
+        return myObj; 
+    } else {
+        return;
+    }
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+    const stringArr = [...string];
+    const calcArray = [];
+    let retString = '';
+    let lastChar = '';
+    let sum = 0;
+    for (const value of stringArr){
+      if (value === lastChar){
+        sum++;
+      } else {
+        sum = 1;
+      }
+      calcArray.push([value, sum]);
+      lastChar = value;
+    }
+    for (const item of calcArray){
+      if (item[1] <= size || size === undefined){
+        retString = retString + item[0];
+      }
+    }
+    return retString;
 }
