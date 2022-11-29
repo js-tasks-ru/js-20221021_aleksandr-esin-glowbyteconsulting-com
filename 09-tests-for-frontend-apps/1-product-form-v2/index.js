69f84467f5d5abc81baf2863d85c6a619b040acc
Aleksandr Esin <aleksandr.esin@glowbyteconsulting.com>
2022-11-29 19:35:15 +0300

    les 10

--- 09-tests-for-frontend-apps/1-product-form-v2/index.js
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -1,4 +1,4 @@
-import SortableList from '../../2-sortable-list/solution';
+import SortableList from '../2-sortable-list/index.js';
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
 
